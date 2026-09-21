c2c98bf78205ca10b7ddb10d35374723dfc35e2c
1-Kiran <kirankumarhosamani2@gmail.com>
2026-09-21 11:12:53 +0530

    Disarium Numbers

--- DSA_Patterns/disarium_no.py
+++ DSA_Patterns/disarium_no.py
@@ -8,4 +8,21 @@ while val>0:
     l-=1
     val=val//10
 if sum==num:
-    print("OK")
+    print("Disarium number")
+
+
+#Print All Disarium Numbers from 1 to 100
+# n=100
+# for i in range(1,n+1):
+#     num=i
+#     val=num
+#     l=len(str(num))
+#     sum=0
+#     while val>0:
+#         one=val%10
+#         sum+=one**(l)
+#         l-=1
+#         val=val//10
+#     if sum==num:
+#         print("Disarium number",sum)
+    
