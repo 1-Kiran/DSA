a805e367d3f7b3fe8c2bf1c22e6b6daef7ab89ff
1-Kiran <kirankumarhosamani2@gmail.com>
2026-09-21 11:14:47 +0530

    Disarium number

--- DSA_Patterns/disarium_no.py
+++ DSA_Patterns/disarium_no.py
@@ -11,6 +11,15 @@ if sum==num:
     print("Disarium number")
 
 
+# A Disarium number is a number equal to the sum of its digits raised to their respective positions.
+
+# Positions start from 1.
+
+# Example
+# 89 = 8¹ + 9²
+#    = 8 + 81
+#    = 89
+
 #Print All Disarium Numbers from 1 to 100
 # n=100
 # for i in range(1,n+1):
@@ -25,4 +34,3 @@ if sum==num:
 #         val=val//10
 #     if sum==num:
 #         print("Disarium number",sum)
-    
