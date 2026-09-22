8c899112ed076211e85ce6d00ab44a63ac288800
1-Kiran <kirankumarhosamani2@gmail.com>
2026-09-22 13:08:48 +0530

    Updated

--- DSA_Patterns/happy_no.py
+++ DSA_Patterns/happy_no.py
@@ -19,11 +19,11 @@ print(seen)
 
 
 #Print All Happy Numbers from 1 to 100
-for i in range(1,101):
-    n=i
-    seen=set()
-    while n!=1 and n not in seen:
-        seen.add(n)
-        n=sum(int(num)**2 for num in str(n))
-        if n==1:
-            print("Happy number")
+# for i in range(1,101):
+#     n=i
+#     seen=set()
+#     while n!=1 and n not in seen:
+#         seen.add(n)
+#         n=sum(int(num)**2 for num in str(n))
+#         if n==1:
+#             print("Happy number")
